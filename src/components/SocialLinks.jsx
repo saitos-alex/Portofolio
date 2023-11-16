d9815b14aa9c403f6f739cf858a87c617438808d
Saitos Alexandru-Cristian <saitos_alex@yahoo.com>
2023-11-16 19:06:25 +0200

    hidden class removed

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -49,7 +49,7 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className="hidden lg:flex flex flex-col top-[35%] left-0 fixed">
+    <div className="lg:flex flex flex-col top-[35%] left-0 fixed">
       <ul>
         {links.map(({ id, child, href, style, download }) => (
           <li
