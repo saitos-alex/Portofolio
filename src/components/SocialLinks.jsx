eba17ec0ba835e1871fd8a42d1277278519edda9
Saitos Alexandru-Cristian <saitos_alex@yahoo.com>
2023-11-16 19:27:06 +0200

    aaa

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -49,7 +49,7 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className="hidden first-letter:lg:flex flex flex-col top-[35%] left-0 fixed">
+    <div className="lg:block  hidden first-letter flex-col top-[35%] left-0 fixed">
       <ul>
         {links.map(({ id, child, href, style, download }) => (
           <li
